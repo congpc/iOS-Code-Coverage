efb620c655be1d45c8fbcefea971bbb59ce4536c
congpc <chicong7891@gmail.com>
2015-08-21 11:04:04 +0700

    Modify build script

--- DemoCodeCoverage/build.sh
+++ DemoCodeCoverage/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # First Run Tests
-xcodebuild test -project DemoCodeCoverage.xcodeproj/ -scheme 'DemoCodeCoverage' -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.3' | xcpretty -c --report junit
+xcodebuild test -project DemoCodeCoverage.xcodeproj/ -scheme 'DemoCodeCoverage' -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 6' | xcpretty -c --report junit
 
 #We set required coverage to 80% - build fails if coverage falls below this value.
 # Now Produce Test Coverage Report
-groovy http://frankencover.it/with -source-dir "DemoCodeCoverage/Classes" -required-coverage 80
+#groovy http://frankencover.it/with -source-dir "DemoCodeCoverage/Classes" -required-coverage 80
 # Use for local
-#groovy frankencover -source-dir "DemoCodeCoverage/Classes" -required-coverage 80
+groovy frankencover -source-dir "DemoCodeCoverage/Classes" -required-coverage 80
 
